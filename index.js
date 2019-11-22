1da29a31c1340c744b00f6a70398dcf2d97433c9
Ceo Epsilon <oprada.epsilon@gmail.com>
2019-11-22 15:40:19 -0300

    Nueva version mejorada con imagen

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-//const element = document.getElementById('excuse');
-//const sum = 2 * 2 * 2;
 
-
-let who = ['the dog', 'my granma', 'his turtle', 'my bird'];
-let what = ['eat', 'pissed', 'crushed', 'broked'];
-let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying'];
+let who = ['El perro', 'Mi Abuela', 'Su tortuga', 'Mi Pajaro'];
+let what = ['se comió', ' la ensució', ' la dañó', ' la rompió'];
+let when = ['antes de clases', 'justo a tiempo', 'Cuando la terminaba', 'mientras almorzaba', 'mientras rezaba'];
 
 
 function generator() {
-    let whoRandom = Math.floor(Math.random() * (who.length));
-    let whoWord = who[whoRandom];
-    let whatRandom = Math.floor(Math.random() * (what.length));
-    let whatWord = what[whatRandom];
-    let whenRandom = Math.floor(Math.random() * (when.length));
-    let whenWord = when[whenRandom];
-    let finalExcuse = console.log(whoWord +" "+ whatWord +" "+ whenWord);
+    let random = Math.floor(Math.random() * (who.length));
+    let whoWord = who[random];
+        random = Math.floor(Math.random() * (what.length));
+    let whatWord = what[random];
+        random = Math.floor(Math.random() * (when.length));
+    let whenWord = when[random];
+    let finalExcuse = ("¡¡¡ "+ whoWord +" "+ whatWord+" "+ whenWord+" !!!");
     return finalExcuse;
 }
 
-//element.innerHTML = generator()
-console.log(generator());
+document.getElementById('excuse').innerHTML = generator();
